88cfb0325207af8b3ac2daff8f1b9f248ee2addd
art123craft <cammiebluecammie@gmail.com>
2018-07-26 21:22:23 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,22 @@
+# x = 13
+# if x > 10
+#   print "I'm the better number!"
+# elsif x == 10
+#   print "I'm the cooler number!"
+# else 
+#   print "I'm the nicer number!"
+# end
+ 
+ time = 7
+ if time >= 7 && time <= 11
+   print "Good Morning."
+   elsif time  >= 11 && time <= 5
+   print "Good Afternoon."
+   elsif time  >= 5 && time <= 10
+   print "Good Evening."
+ else
+   print "Good Night."
+ end
+
+
+
--- trip.rb
+++ trip.rb
@@ -1,16 +1,11 @@
-# Code your prompts here!
+puts "Let's plan your trip! Where would you like to stay on your trip?"
+stay = gets.chomp.capitalize 
 
-# First, puts out a string asking where the tourist would like to stay.
-puts "Let's plan your trip! Where would you like to stay?"
-
-# Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
-
-stay = gets.chomp.capitalize  #don't forget to Capitilize the input
-
-# Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
+puts "What would you like to eat?"
+eat = gets.chomp
 
 
+puts "How many nights would you like to stay?"
+nights = gets.chomp
 
-# Lastly, puts it all back by interpolating these values in a string.
-
-puts ""
+puts "So you would like to go to #{stay}, go eat #{eat}, and stay #{nights}."
